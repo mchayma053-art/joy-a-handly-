143cba10ad37a6dc8e7564208e12419a4b23359c
Mansouri Chayma <you@example.com>
2026-04-17 13:25:42 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.6 (https://Joy’A❣️_handly.com/template/Joy’A❣️_handly)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/shop-homepage/shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
